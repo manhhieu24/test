b7e2c71f8e0e2faba5babf94195ff1eac20c1b30
manhhieu24 <manhhieu24@gmail.com>
2024-06-25 19:40:39 +0700

    replacye

--- demo/settings.py
+++ demo/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR / "app/static"),
-STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles_build" / "app/static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
